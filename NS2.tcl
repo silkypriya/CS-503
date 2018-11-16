2e4b1e5072cb57f42a4b865ca678ed0e56d28e06
Silky Priya <silkypriya64@gmail.com>
2018-11-16 12:01:55 +0530

    Update NS2.tcl

--- NS2.tcl
+++ NS2.tcl
@@ -5,3 +5,56 @@ $ns color 2 Red
 
 set val(x) 500
 set val(y) 500
+
+# Define options
+set val(chan) Channel/WirelessChannel    ;# channel type
+set val(prop) Propagation/TwoRayGround   ;# radio-propagation model
+
+set val(netif)          Phy/WirelessPhy            ;# network interface type
+set val(mac)            Mac/802_11                 ;# MAC type
+set val(ifq)            Queue/DropTail/PriQueue    ;# interface queue type. This can be changed.
+set val(ll)             LL                         ;# link layer type
+set val(ant)            Antenna/OmniAntenna        ;# antenna model
+set val(ifqlen)         50                         ;# max packet in ifq
+set val(nn)             2                      ;# number of mobilenodes
+set val(rp)             AODV                       ;# routing protocol
+set val(x)              500   			   ;# X dimension of topography
+set val(y)              400   			   ;# Y dimension of topography  
+set val(stop)		10.0			   ;# time of simulation end
+
+# set up topography object
+set topo       [new Topography]
+$topo load_flatgrid $val(x) $val(y)
+
+#Nam File Creation  nam - network animator
+set namfile [open sample1.nam w]
+
+#Tracing all the events and cofiguration
+$ns namtrace-all-wireless $namfile $val(x) $val(y)
+
+#Trace File creation
+set tracefile [open sample1.tr w]
+
+#Tracing all the events and cofiguration
+$ns trace-all $tracefile
+
+# general operational descriptor- storing the hop details in the network
+create-god $val(nn)
+
+# configure the nodes
+        $ns node-config -adhocRouting $val(rp) \
+			 -llType $val(ll) \
+			 -macType $val(mac) \
+			 -ifqType $val(ifq) \
+			 -ifqLen $val(ifqlen) \
+			 -antType $val(ant) \
+			 -propType $val(prop) \
+			 -phyType $val(netif) \
+			 -channelType $val(chan) \
+			 -topoInstance $topo \
+			 -agentTrace ON \
+			 -routerTrace ON \
+			 -macTrace OFF \
+			 -movementTrace ON
+
+
